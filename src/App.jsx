d8c857146bda816a058946d66376850169ec5b22
ALI-ADAM94 <aliadam081194@gmail.com>
2025-04-20 11:28:11 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+        <h1 class="text-3xl font-bold underline bg-gray-400">
+    Hello world!
+  </h1>
+
+    </>
+  )
+}
+
+export default App
